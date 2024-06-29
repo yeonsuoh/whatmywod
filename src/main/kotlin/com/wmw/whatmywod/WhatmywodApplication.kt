211bf525e35b21600fa9c462b5468fb62fd489d6
lilylemonoh <lilylemonoh@naver.com>
2024-06-29 22:03:28 +0900

    project init

---
+++ src/main/kotlin/com/wmw/whatmywod/WhatmywodApplication.kt
@@ -0,0 +1,11 @@
+package com.wmw.whatmywod
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WhatmywodApplication
+
+fun main(args: Array<String>) {
+	runApplication<WhatmywodApplication>(*args)
+}
